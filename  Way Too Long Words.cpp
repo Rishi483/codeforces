db125cd3d374b7ebf8ca37dcc18c2cf8c6f46f59
Rishi483 <97284468+Rishi483@users.noreply.github.com>
2022-04-18 10:11:43 +0530

    Add files via upload

---
+++  Way Too Long Words.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define end1 "\n"
+
+
+int gcd(int a, int b)
+{
+   return __gcd(a,b);
+}
+
+void solve(){
+  string s;
+  cin>>s;
+  int n=s.length();
+  char i=s[0];
+  char e=s[n-1];
+  if(n>10){
+  cout<<i<<n-2<<e<<endl;
+  }
+  else{
+      cout<<s<<endl;
+  }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
