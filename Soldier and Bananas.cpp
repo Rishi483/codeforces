bd85279b0f9bee5367ad319080360aa25824a554
Rishi483 <97284468+Rishi483@users.noreply.github.com>
2022-04-18 20:26:06 +0530

    Add files via upload

---
+++ Soldier and Bananas.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define end1 "\n"
+
+
+int gcd(int a, int b)
+{
+   return __gcd(a,b);
+}
+
+void solve(){
+  int k,n,w;
+  cin>>k>>n>>w;
+  int x=(w*(w+1))/2;
+  x*=k;
+  if(x-n>0){
+  cout<<x-n<<endl;
+  }
+  else{
+      cout<<0<<endl;
+  }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	
+	    solve();
+	
+	return 0;
+}
