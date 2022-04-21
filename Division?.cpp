2da914550c55e461b402dd0dd5ae496b3a4b1334
Rishi483 <97284468+Rishi483@users.noreply.github.com>
2022-04-21 22:15:14 +0530

    Add files via upload

---
+++ Division?.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define end1 "\n"
+
+
+int gcd(int a, int b)
+{
+   return __gcd(a,b);
+}
+
+void solve(){
+  int n;
+  cin>>n;
+  if(n>=1900) cout<<"Division 1"<<endl;
+  else if(1600<=n && n<1900) cout<<"Division 2"<<endl;
+  else if(n>=1400 && n<=1599) cout<<"Division 3"<<endl;
+  else if(n<=1399) cout<<"Division 4"<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
