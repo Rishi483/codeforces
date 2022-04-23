a97f288a4cdfe473c895db68828e3531552fffe1
Rishi483 <97284468+Rishi483@users.noreply.github.com>
2022-04-23 18:48:54 +0530

    Add files via upload

---
+++ Theatre Square.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define end1 "\n"
+
+
+int gcd(int a, int b)
+{
+   return __gcd(a,b);
+}
+
+void solve(){
+  int m,n,a;
+  cin>>m>>n>>a;
+  int x=m/a;
+  int y=n/a;
+  if(m%a!=0) x++;
+  if(n%a!=0) y++;
+  cout<<x*y<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+
+	    solve();
+	
+	return 0;
+}
